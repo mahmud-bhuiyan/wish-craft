df8dec56ee4ce530c56302186175348d8a545b44
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-12-23 18:46:09 +0600

    auth layout and login page added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import AuthLayout from "../layout/AuthLayout";
 import Home from "../pages/Home";
 import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +14,17 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
