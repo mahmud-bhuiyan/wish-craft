b8bef26de6a853c6cd1a09ba0dc4bf5da7285170
mahmud-bhuiyan <bh.mahmud@gmail.com>
2024-01-13 17:06:33 +0600

    search features design updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,4 @@
-// import { useState, useContext } from "react";
-// import { Helmet } from "react-helmet-async";
-// import { MdSearch } from "react-icons/md";
-// import { FeaturesContext } from "../context/FeaturesContextProvider";
-// import FeatureRequestItem from "../components/Feature/FeatureRequestItem";
-// import Loader from "../components/Loader";
-
-// const Home = () => {
-//   const { features, loading } = useContext(FeaturesContext);
-//   const [searchTerm, setSearchTerm] = useState("");
-
-//   // Filtering function
-//   const filterFeatures = (feature) => {
-//     const searchRegex = new RegExp(searchTerm, "i");
-//     return (
-//       searchRegex.test(feature.title) || searchRegex.test(feature.description)
-//     );
-//   };
-
-//   // Filtered features based on the search term
-//   const filteredFeatures = features.filter(filterFeatures);
-
-//   return (
-//     <div>
-//       <Helmet>
-//         <title>WishCraft</title>
-//       </Helmet>
-
-//       <div className="max-w-screen-xl mx-auto">
-//         <div className="mx-2 my-4">
-//           <div className="lg:flex bg-white rounded-lg p-1">
-//             {/* Content for the left div */}
-//             <div className="lg:w-1/3 lg:order-first p-4 border-2 m-3 rounded-lg">
-//               <p>Left Column</p>
-//             </div>
-
-//             {/* Content for the right div */}
-//             <div className="flex-1">
-//               {/* Search bar with icon */}
-//               <div className="relative m-3">
-//                 <MdSearch className="absolute left-3 top-2.5 text-gray-500 text-2xl" />
-//                 <input
-//                   type="text"
-//                   placeholder="Search by keywords..."
-//                   value={searchTerm}
-//                   onChange={(e) => setSearchTerm(e.target.value)}
-//                   className="p-2 pl-10 border rounded w-full"
-//                 />
-//               </div>
-//               <div className="p-4 border-2 m-3 rounded-lg">
-//                 {loading ? (
-//                   <Loader />
-//                 ) : (
-//                   filteredFeatures.map((feature) => (
-//                     <FeatureRequestItem key={feature._id} feature={feature} />
-//                   ))
-//                 )}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { MdSearch } from "react-icons/md";
 import { FeaturesContext } from "../context/FeaturesContextProvider";
@@ -73,15 +6,8 @@ import FeatureRequestItem from "../components/Feature/FeatureRequestItem";
 import Loader from "../components/Loader";
 
 const Home = () => {
-  // const { features, loading, searchFeatures } = useContext(FeaturesContext);
-
   const { features, loading, searchTerm, searchResults, setSearchTerm } =
     useContext(FeaturesContext);
-
-  // useEffect(() => {
-  //   // Trigger the search with the entered searchTerm
-  //   searchFeatures(searchTerm);
-  // }, [searchTerm, searchFeatures]);
 
   return (
     <div>
@@ -111,13 +37,6 @@ const Home = () => {
                 />
               </div>
               <div className="p-4 border-2 m-3 rounded-lg">
-                {/* {loading ? (
-                  <Loader />
-                ) : (
-                  features.map((feature) => (
-                    <FeatureRequestItem key={feature._id} feature={feature} />
-                  ))
-                )} */}
                 {loading ? (
                   <Loader />
                 ) : searchTerm && searchResults.length > 0 ? (
